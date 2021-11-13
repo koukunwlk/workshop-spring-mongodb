31dbe596f8cca0fca6eb8410185e29e7de2e8b2f
Moacir Amaro <moacir@gmail.com>
2021-11-13 17:43:05 -0300

    Refactoring Post with AuthorDTO

--- src/main/java/com/moaciramaro/workshopmongo/config/Instantiation.java
+++ src/main/java/com/moaciramaro/workshopmongo/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.moaciramaro.workshopmongo.config;
 
 import com.moaciramaro.workshopmongo.domain.Post;
 import com.moaciramaro.workshopmongo.domain.User;
+import com.moaciramaro.workshopmongo.dto.AuthorDTO;
 import com.moaciramaro.workshopmongo.repository.PostRepository;
 import com.moaciramaro.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,11 +33,12 @@ public class Instantiation implements CommandLineRunner {
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para Canada", maria);
-        Post post2 = new Post(null, sdf.parse("22/03/2018"), "Partiu viagem", "Vou viajar para Portugal", maria);
-        Post post3 = new Post(null, sdf.parse("23/03/2018"), "Partiu viagem", "Vou viajar para Reino Unido", maria);
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+        
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para Canada", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("22/03/2018"), "Partiu viagem", "Vou viajar para Portugal", new AuthorDTO(maria));
+        Post post3 = new Post(null, sdf.parse("23/03/2018"), "Partiu viagem", "Vou viajar para Reino Unido", new AuthorDTO(maria));
 
         postRepository.saveAll(Arrays.asList(post1, post2, post3));
-        userRepository.saveAll(Arrays.asList(maria, alex, bob));
     }
 }
