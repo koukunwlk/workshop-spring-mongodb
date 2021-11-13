a2283a2bc87685dbfc8c7a25a6be8251043c1867
Moacir Amaro <moacir@gmail.com>
2021-11-13 14:37:04 -0300

    Add db population config

---
+++ src/main/java/com/moaciramaro/workshopmongo/config/Instantiation.java
@@ -0,0 +1,27 @@
+package com.moaciramaro.workshopmongo.config;
+
+import com.moaciramaro.workshopmongo.domain.User;
+import com.moaciramaro.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository repository;
+    @Override
+    public void run(String... args) throws Exception {
+
+        repository.deleteAll();
+
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        repository.saveAll(Arrays.asList(maria, alex, bob));
+    }
+}
