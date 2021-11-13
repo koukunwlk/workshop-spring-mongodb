afdf34fdbdf83135828cbaf696e8e1ea5ee77dcd
Moacir Amaro <moacir@gmail.com>
2021-11-13 15:42:48 -0300

    Add post new user

--- src/main/java/com/moaciramaro/workshopmongo/services/UserService.java
+++ src/main/java/com/moaciramaro/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.moaciramaro.workshopmongo.services;
 
 import com.moaciramaro.workshopmongo.domain.User;
+import com.moaciramaro.workshopmongo.dto.UserDTO;
 import com.moaciramaro.workshopmongo.repository.UserRepository;
 import com.moaciramaro.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> user = repository.findById(id);
         return  user.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+    }
+
+    public User insert(User user) {
+        return repository.insert(user);
+    }
+
+    public User fromDTO(UserDTO userDto) {
+        return new User(userDto.getId(), userDto.getName(), userDto.getEmail());
     }
 }
